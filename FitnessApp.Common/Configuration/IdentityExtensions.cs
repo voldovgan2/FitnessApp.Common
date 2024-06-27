dcf5d92e2a980a88895b9016bf66863420a23469
volodymyr.dovhan <volodymyr.dovhan@steel.mil.ua>
2024-06-27 17:47:16 +0300

    Enhance identity configuration

--- FitnessApp.Common/Configuration/IdentityExtensions.cs
+++ FitnessApp.Common/Configuration/IdentityExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FitnessApp.Common.Configuration
 {
@@ -8,17 +9,18 @@ namespace FitnessApp.Common.Configuration
     {
         public static IServiceCollection ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddAuthentication(opts =>
-            {
-                opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                opts.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
+            services
+                .AddAuthentication("Bearer")
                 .AddJwtBearer(cfg =>
                 {
                     cfg.RequireHttpsMetadata = false;
                     cfg.Authority = configuration["OpenIdConnect:Issuer"];
                     cfg.Audience = configuration["OpenIdConnect:Audience"];
+                    cfg.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidAudience = configuration["OpenIdConnect:Audience"],
+                        ValidIssuer = configuration["OpenIdConnect:Issuer"]
+                    };
                 });
             return services;
         }
