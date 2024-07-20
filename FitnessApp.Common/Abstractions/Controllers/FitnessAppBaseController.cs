d39b0f865e012b13e6212960d580a9e5796bee55
Volodimir <volodimir.dovgan@hotmail.com>
2024-07-20 18:30:38 +0300

    add base controller

---
+++ FitnessApp.Common/Abstractions/Controllers/FitnessAppBaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessApp.Common.Abstractions.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces("application/json")]
+
+[Authorize]
+public abstract class FitnessAppBaseController : Controller;
