945811ed34a04d722b9e13fe45f9bf17d2bbf23c
Volodimir <volodimir.dovgan@hotmail.com>
2024-07-29 09:04:44 +0300

    add WithNameAndDescription

---
+++ FitnessApp.Common/Abstractions/Models/Collection/ICollectionItemWithNameAndDescriptionModel.cs
@@ -0,0 +1,6 @@
+﻿namespace FitnessApp.Common.Abstractions.Models.Collection;
+public interface ICollectionItemWithNameAndDescriptionModel : ICollectionItemModel
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
