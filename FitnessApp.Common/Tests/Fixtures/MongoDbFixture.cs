5f53c605d56ed0e695afb1809866fb6eaf3c26cf
Volodimir <volodimir.dovgan@hotmail.com>
2024-11-19 22:08:04 +0200

    restore tests

---
+++ FitnessApp.Common/Tests/Fixtures/MongoDbFixture.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace FitnessApp.Common.Tests.Fixtures;
+public abstract class MongoDbFixture<TEntity> : IDisposable
+    where TEntity : class
+{
+    protected IMongoCollection<TEntity> Collection { get; }
+
+    protected MongoDbFixture(
+        string connectionString,
+        string databaseName,
+        string collecttionName)
+    {
+        var database = new MongoClient(connectionString).GetDatabase(databaseName);
+        if (!database.ListCollectionNames().ToList().Exists(c => c == collecttionName))
+            database.CreateCollection(collecttionName);
+        Collection = database.GetCollection<TEntity>(collecttionName);
+    }
+
+    public async Task SeedData(TEntity[] items)
+    {
+        var createdItemsTasks = items.Select(item => Collection.InsertOneAsync(item));
+        await Task.WhenAll(createdItemsTasks);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+            Collection.DeleteMany(e => true);
+    }
+}
---
+++ FitnessApp.Common/Tests/Fixtures/TestWebApplicationFactory.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FitnessApp.Common.Tests.Fixtures;
+
+public class TestWebApplicationFactory<
+    TProgram,
+    TAuthenticationHandler,
+    TEntity>(MongoDbFixture<TEntity> fixture, TEntity[] items) :
+    WebApplicationFactory<TProgram>
+    where TProgram : class
+    where TAuthenticationHandler : MockAuthenticationHandlerBase
+    where TEntity : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder
+            .ConfigureTestServices(services =>
+            {
+                services
+                    .AddAuthentication(defaultScheme: MockConstants.Scheme)
+                    .AddScheme<AuthenticationSchemeOptions, TAuthenticationHandler>(MockConstants.Scheme, options => { });
+            })
+            .UseEnvironment("Development");
+        fixture.SeedData(items).GetAwaiter().GetResult();
+    }
+
+    public HttpClient CreateHttpClient()
+    {
+        var httpClient = CreateClient();
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(scheme: MockConstants.Scheme);
+        return httpClient;
+    }
+}
